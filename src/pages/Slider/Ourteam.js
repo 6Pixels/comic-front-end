1507d142423b07a6e961ea1348a0ca5e62090291
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-07-28 19:02:20 +0300

    finish home page

---
+++ src/pages/Slider/Ourteam.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+
+
+export class Ourteam extends Component {
+
+
+
+    render() {
+
+        return (
+            <>
+                <h1>Meet Our Team
+                </h1>
+                <p>- WE ARE STRONGER -
+                </p>
+
+                <section class="slider">
+
+                    <div class="wrapt wrap--1">
+                        <div class="containert1 container--1">
+                        </div>
+                        <h6>Dena</h6>
+                        <p>Kofahi</p>
+                    </div>
+
+                    <div class="wrapt wrap--2">
+                        <div class="containert2 container--2">
+                        </div>
+                        <h6>Tariq</h6>
+                        <p> Abu-Laban</p>
+                    </div>
+
+                    <div class="wrapt wrap--3">
+                        <div class="containert3 container--3">
+                        </div>
+                        <h6>Sondos</h6>
+                        <p> Al-jawhary</p>
+                    </div>
+
+                    <div class="wrapt wrap--1">
+                        <div class="containert4 container--1">
+                        </div>
+                        <h6>Khaled</h6>
+                        <p> Tahat</p>
+                    </div>
+
+                    <div class="wrapt wrap--2">
+                        <div class="containert5 container--2">
+                        </div>
+                        <h6>LAITH</h6>
+                        <p> HAYAJNEH</p>
+                    </div>
+
+                    <div class="wrapt wrap--3">
+                        <div class="containert6 container--3">
+                        </div>
+                        <h6>Mohammad</h6>
+                        <p> Alkhateeb</p>
+                    </div>
+
+                </section>
+
+
+            </>
+        )
+    }
+}
+
+export default Ourteam
